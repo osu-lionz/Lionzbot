4befe3d3bafef51973f78191a6b7f514182e2de2
Lionz <96448965+lionzv2@users.noreply.github.com>
2022-05-10 14:05:36 +0200

    lol big brain, don't bother stealing the token I changed it lol

--- Config.js
+++ Config.js
@@ -1,9 +1,9 @@
 module.exports = {
     prefix: ["bot! "],
-    developers: ["673304389875925003"],
-    token: "ODIzMjc2MTU2NzE5MjAyMzE0.YFedqw.vEYXbElBRfd7yAZ0RsMXTC4Q374",
+    developers: [""],
+    token: "",
     apidomain: "https://api.leeuwz.nl/",
-    apikey: "c1271b0902243472f0fdd829e80721cf102e533f",
+    apikey: "",
     domain: "https://web.leeuwz.nl/",
     discord: "https://discord.com/invite/Yd9ekxuJAw",
     logourl: "https://i.imgur.com/2xrQbSP.png",
